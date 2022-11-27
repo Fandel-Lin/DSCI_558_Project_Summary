64168faa5dca79d35db3288e1fe05a0310fa2989
zhan22 <98624130+zhan22@users.noreply.github.com>
2022-11-26 22:35:29 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,47 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+
+def main():
+    page = st.sidebar.selectbox(
+        "Select a Page",
+        [
+            "Homepage",
+            "Search",
+            "Predict",
+            "Player Information",
+            "Scheduel"
+
+        ]
+    )
+
+    #First Page
+    if page == "Homepage":
+        homepage()
+#     #Search Page
+#     if page == "Search":
+#         Search()
+#     #Predict Page
+#     if page == "Predict":
+#         Predict()
+#     #Player Information Page
+#     if page == "Player Information":
+#         Player()
+
+        
+        
+        
+        
+def homepage():
+    st.write("""
+            # MLB KG
+            #### This project is about to build a knowledge graph for MLB.
+            ###### This app was created by _**Zihao H., Fandel Lin.**_
+            #""")
+
+
+  
 
